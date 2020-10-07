84484fe7c80c77d4ddf4533bbded6aac52ac8698
Andrew Collier <andrew@exegetic.biz>
2020-10-07 11:59:17 +0200

    Add webshot exercise

---
+++ 02-exercise-screenshot-dplyr.R
@@ -0,0 +1,42 @@
+# =====================================================================================================================
+#  _____                                     _             _        
+# /  ___|                                   | |           | |       
+# \ `--.   ___  _ __  ___   ___  _ __   ___ | |__    ___  | |_  ___ 
+#  `--. \ / __|| '__|/ _ \ / _ \| '_ \ / __|| '_ \  / _ \ | __|/ __|
+# /\__/ /| (__ | |  |  __/|  __/| | | |\__ \| | | || (_) || |_ \__ \
+# \____/  \___||_|   \___| \___||_| |_||___/|_| |_| \___/  \__||___/                                             [doom]
+#
+# Web Scraping: Screenshots                   Exegetic Analytics / www.exegetic.biz / info@exegetic.biz / @exegeticdata
+# =====================================================================================================================
+
+# LIBRARIES -----------------------------------------------------------------------------------------------------------
+
+# Load the webshot library.
+
+# === -> YOUR CODE ===
+# === <- YOUR CODE ===
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+# 1. Take a screenshot of https://dplyr.tidyverse.org/.
+# 2. Use the zoom parameter to create a higher resolution screenshot.
+# 3. Use the selector parameter to take a screenshot of the contents of the "Dev status" <div>.
+#
+# Hint: The CSS selector for the "Dev status" <div> is ".dev-status".
+
+# === -> YOUR CODE ===
+# === <- YOUR CODE ===
+
+# BONUS: ANOTHER INTERESTING SITE -------------------------------------------------------------------------------------
+
+# Take a screenshot of another interesting site.
+
+# === -> YOUR CODE ===
+# === <- YOUR CODE ===
+
+# BONUS: SERIES OF SCREENSHOTS ----------------------------------------------------------------------------------------
+
+# Automate screenshots from a series of sites or pages.
+
+# === -> YOUR CODE ===
+# === <- YOUR CODE ===
